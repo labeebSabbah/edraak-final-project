f8f0b4fad134eb7aca2b0d81259faa57bdfe4eb9
Labeeb Sabbah <labeebmuath2003@gmail.com>
2022-05-28 16:13:17 +0300

    Added Categories

--- database/migrations/2022_05_28_125807_create_categories_table.php
+++ database/migrations/2022_05_28_125807_create_categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->string("name")->unique();
+            $table->boolean("is_main");
         });
     }
 

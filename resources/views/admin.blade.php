bc4addde8d1cf054c050bdccf9f2151c7c9edb8f
Labeeb Sabbah <labeebmuath2003@gmail.com>
2022-05-29 13:03:35 +0300

    Changes

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -12,17 +12,37 @@ $items = DB::select('SELECT * FROM categories WHERE is_main = true');
 </head>
 <body>
     <h2>Main Categories</h2>
-    <form action="/create" method="get">
-        <input type="text" name="name" placeholder="Add Product">
+    <form action="/create" method="get" id="mainForm">
+        <p id="message"></p>
+        <input type="text" name="name" placeholder="Add Product" id="mainName">
         <button type="submit" name="main" value="1">Add</button> <br>
     </form>
     <ol>
     <?php 
             foreach ($items as $item) {
                 $name = $item->name;
-                echo "<li name='{$name}'>{$name}</li>";
+                echo "<li name='{$name}' class='mainNames' value='{$name}'>{$name}</li>";
             }
         ?>
     </ol>
+    <script>
+        const mainForm = document.getElementById('mainForm');
+        const mainName = document.getElementById('mainName');
+        const messages = document.getElementById('message');
+        const mainNames = document.getElementsByClassName('mainNames');
+        
+        mainForm.addEventListener("submit", function (e) {
+            let error = '';
+            for (let i = 0; i < mainNames.length; i++) {
+                if(mainName.value == mainNames[i].value)  {
+                    error += mainName.value + "Already Exists";
+                    if (errro) {
+                        e.preventDefault();
+                        messages.innerHTML = error;
+                    }
+                }
+            }
+        });
+    </script>
 </body>
 </html>

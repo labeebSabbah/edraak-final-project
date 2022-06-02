f62c5a57e43f3477d0ebd59083a85658e0bbafe0
Labeeb Sabbah <labeebmuath2003@gmail.com>
2022-06-02 18:05:27 +0300

    Edit Category Added

--- resources/views/categories/updateCat.blade.php
+++ resources/views/categories/updateCat.blade.php
@@ -1,3 +1,8 @@
+<?php 
+	use Illuminate\Support\Facades\DB;
+
+	$cats = DB::select('SELECT * FROM categories');
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,5 +15,31 @@
 		<input type="text" name="new_name" id="name">
 		<button type="submit" name="name" value='{{$name}}'>Update</button>
 	</form>
+	<script type="text/javascript">
+
+		const new_name = document.getElementById('name');
+		const updateForm = document.getElementById('updateForm');
+
+		updateForm.addEventListener('submit', function() {
+			let error = '';
+			if (new_name.value == "") {
+				error += "Enter A Name";
+			} else {
+				@foreach ($cats as $item)
+				if (new_name.value == {{$item->name}}) {
+					error += $new_name.value + " Already Exists";
+					break;
+				}
+				@endforeach
+			}
+			if (error) {
+				alert(error);
+			} else {
+				alert("Updated Successfully");
+			}
+
+		});
+
+	</script>
 </body>
 </html>

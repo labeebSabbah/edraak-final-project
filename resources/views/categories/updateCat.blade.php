01ec207527ed1a44ce9fc904551b5ce42ea225cb
Labeeb Sabbah <labeebmuath2003@gmail.com>
2022-06-02 18:14:14 +0300

    Edit Category Added

--- resources/views/categories/updateCat.blade.php
+++ resources/views/categories/updateCat.blade.php
@@ -12,26 +12,27 @@
 </head>
 <body>
 	<form method="get" action="/updateCat" id="updateForm">
-		<input type="text" name="new_name" id="name">
+		<input type="text" name="new_name" id="new_name">
 		<button type="submit" name="name" value='{{$name}}'>Update</button>
 	</form>
 	<script type="text/javascript">
 
-		const new_name = document.getElementById('name');
+		const new_name = document.getElementById('new_name');
 		const updateForm = document.getElementById('updateForm');
 
 		updateForm.addEventListener('submit', function(e) {
-			let error = '';
+			var error = '';
 			if (new_name.value == "") {
 				error += "Enter A Name";
 			} else {
-				@foreach ($cats as $item)
-				if (new_name.value == {{$item->name}}) {
-					error += $new_name.value + " Already Exists";
-					break;
-				}
-				@endforeach
+				// @foreach ($cats as $item)
+				// if (new_name.value == {{$item->name}}) {
+				// 	error += $new_name.value + " Already Exists";
+				// 	break;
+				// }
+				// @endforeach
 			}
+
 			if (error) {
 				e.preventDefault();
 				alert(error);

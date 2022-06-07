f07a47799de4e754c95c8a4c0c8c0c5029f6440c
Labeeb Sabbah <labeebmuath2003@gmail.com>
2022-06-07 11:16:50 +0300

    Delete Product Added

--- resources/views/products/showProducts.blade.php
+++ resources/views/products/showProducts.blade.php
@@ -19,5 +19,23 @@
 
 	{{ $products->links() }}
 
+	<script type="text/javascript">
+
+		const deleteForm = document.getElementById('deleteForm');
+
+		deleteForm.addEventListener('submit', function (e) {
+			var error = '';
+			var choice = confirm('Are You Sure That You Want To Delete It ?');
+			if (!choice) {
+				e.preventDefault();
+			} else {
+				alert("Category Deleted Successfully");
+			}
+			if (error) {
+				alert(error);
+			}
+		});
+	</script>
+
 </body>
 </html>

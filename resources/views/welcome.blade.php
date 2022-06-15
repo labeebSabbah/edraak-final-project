eef96fcbb93657fd438a3a473d73acdc1edd9a6d
Labeeb Sabbah <labeebmuath2003@gmail.com>
2022-06-15 18:23:01 +0300

    Main Page Modified

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,18 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./css/app.css">
     <title>Labeeb's E-Commerce</title>
 </head>
 <body>
     <h1>Hello World</h1>
     <a href="/login">Login</a>
     <a href="/register">Register</a>
-    <div style="margin: auto;">
+    <div style="margin: 0 auto;">
         @foreach ($mainCats as $item)
         <span>{{$item->name}} </span>
         @endforeach
     </div>
-    <div style="margin: auto;">
+    <div style="margin: 0 auto;">
         @foreach ($products as $item)
         <span>{{$item->name}} </span>
         @endforeach

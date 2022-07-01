75b9dd9ff17b5f2ace045cb31b3886368a39c4a2
Labeeb Sabbah <labeebmuath2003@gmail.com>
2022-07-01 14:33:19 +0300

    Style Added

--- resources/views/categories/showCategories.blade.php
+++ resources/views/categories/showCategories.blade.php
@@ -8,29 +8,41 @@
 </head>
 <body>
 	@include('components.admin-nav')
-	<a href="/createMain">Add</a>
-	<ol>
-		<form method="get" action="/deleteCat" class="deleteForms">
-			@foreach ($mainCat as $item)
-				<li name='{{$item->name}}' class='names'>{{$item->name}}</li><a href="/createSub?id={{$item->id}}">Add</a>
-				<button type='submit' name='name' value='{{$item->id}}'>Delete</button>
-				<a href="/updateCat/{{$item->name}}">Update</a>
-			@endforeach
-		</form>
-	</ol>
-	<ol>
-		<form method="get" action="/deleteCat" class="deleteForms">
-			@foreach ($subCat as $item)
-				<li name='{{$item->name}}' class='names'>{{$item->name}}</li>
-				<button type='submit' name='name' value='{{$item->name}}'>Delete</button>
-				<a href='/updateCat/{{$item->name}}'>Update</a>
-			@endforeach
-		</form>
-	</ol>
+
+	<div class="flex justify-center">
+  			<ul class="bg-white rounded-lg border border-gray-200 text-gray-900 " style="width: 75%; margin-left: 15em;">
+  				<li class="px-6 py-2 border-b border-gray-200 w-full"><a href="/createMain"><button>Add</button></a></li>
+  				<form method="get" action="/deleteCat" id="deleteForm">
+    			@foreach ($mainCat as $main)
+    			<li class="px-6 py-2 border-b border-gray-200 w-full">
+    				<span>{{$main->id}}</span>
+    				<span style="margin-left: 20px;">{{$main->name}}</span>
+    				<span style="float: right; display: block;">
+    					<a href="/createSub?id={{$main->id}}"><button>Add</button></a>
+    					<a href="/updateCat/{{$main->name}}"><button>Update</button></a>
+    					<button type='submit' name='name' value='{{$main->id}}'>Delete</button>
+    				</span>
+    				<ul>
+    					@foreach ($subCat as $sub)
+    					@if ($sub->main_id == $main->id)
+    					<li>
+    						<span style="margin-left: 20px;">{{$sub->name}}</span>
+    						<span style="float: right; display: block;">
+    							<a href="/updateCat/{{$sub->name}}"><button>Update</button></a>
+    							<button type='submit' name='name' value='{{$sub->id}}'>Delete</button>
+    						</span>
+    					</li>
+    					@endif
+    					@endforeach
+    				</ul>
+    			</li>
+    			@endforeach
+    			</form>
+  			</ul>
+	</div>
 
 	<script type="text/javascript">
 
-			const names = document.getElementsByClassName('names');
 			const deleteForms = document.getElementsByClassName('deleteForms');
 
 			for (let i = 0; i < deleteForms.length; i++) {

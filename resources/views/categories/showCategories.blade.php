fb9b48f6988adf80c8348c3be6b7d3d24e7af19f
Labeeb Sabbah <labeebmuath2003@gmail.com>
2022-07-05 12:22:26 +0300

    Images Fixed

--- resources/views/categories/showCategories.blade.php
+++ resources/views/categories/showCategories.blade.php
@@ -7,9 +7,7 @@
 	<link rel="stylesheet" type="text/css" href="./css/app.css">
 	<style type="text/css">
 		button {
-			padding: 6px;
-			border: 2px solid #06283D;
-			color: #DFF6FF;
+			margin-right: 10px;
 		}
 	</style>
 </head>
@@ -18,16 +16,16 @@
 
 	<div class="flex justify-center">
   			<ul class="rounded-lg border border-gray-200 text-gray-900 " style="width: 75%; margin-left: 15em; background: #47B5FF;">
-  				<li class="px-6 py-2 border-b border-gray-200 w-full"><a href="/createMain"><button style= "background-color: #06283D;">Add</button></a></li>
+  				<li class="px-6 py-2 border-b border-gray-200 w-full"><a href="/createMain"><button >Add</button></a></li>
   				<form method="get" action="/deleteCat" id="deleteForm">
     			@foreach ($mainCat as $main)
     			<li class="px-6 py-2 border-b border-gray-200 w-full">
     				<span>{{$main->id}}</span>
     				<span style="margin-left: 20px;">{{$main->name}}</span>
     				<span style="float: right; display: block;">
-    					<a href="/createSub?id={{$main->id}}"><button type="button" style= "background-color: #06283D;">Add</button></a>
-    					<a href="/updateCat/{{$main->name}}"><button type="button" style= "background-color: #06283D;">Update</button></a>
-    					<button type='submit' name='name' value='{{$main->id}}' style= "background-color: #06283D;">Delete</button>
+    					<a href="/createSub?id={{$main->id}}"><button type="button" >Add</button></a>
+    					<a href="/updateCat/{{$main->name}}"><button type="button" >Update</button></a>
+    					<button type='submit' name='name' value='{{$main->id}}' >Delete</button>
     				</span>
     				<ul style="margin: auto;margin-top: 0.5em; width: 70%;">
     					@foreach ($subCat as $sub)
@@ -35,8 +33,8 @@
     					<li>
     						<span style="margin-left: 20px;">{{$sub->name}}</span>
     						<span style="float: right; display: block;">
-    							<a href="/updateCat/{{$sub->name}}"><button type="button" style= "background-color: #06283D;">Update</button></a>
-    							<button type='submit' name='name' value='{{$sub->name}}' style= "background-color: #06283D;">Delete</button>
+    							<a href="/updateCat/{{$sub->name}}"><button type="button" >Update</button></a>
+    							<button type='submit' name='name' value='{{$sub->name}}' >Delete</button>
     						</span>
     					</li>
     					@endif
